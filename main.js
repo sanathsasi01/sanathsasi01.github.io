94ec3d1f162e3dbaed70349c9336d7a0ecdccfe1
sanath sasi <sanathsasi01@gmail.com>
2021-12-06 15:09:06 +0530

    projects

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
-var i = 0;
-var txt = "And, I'm a Software Developer!";
-var speed = 50;
+
+// var txt = "And, I'm a Software Developer!";
+var txt = "                    Graduate in Computer Science & Engineering and a Software Developer!";
+var speed = 20;
 
 window.onload = function() {
     typeWriter()
 }
-
+var anchorTag = document.getElementsByTagName('a');
+for (var i = 0; i < anchorTag.length; i++) {
+  // anchorTag[i].addEventListener('click', function(e) {
+  //   e.preventDefault()
+  // })
+}
 function typeWriter(){
     if (i < txt.length) {
         document.getElementById("typingText").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }else {
+      setTimeout(function() {
         document.getElementById("arrow").style.visibility = "visible";
+      }, 300);
+      setTimeout(function() {
         document.getElementById("resume").style.visibility = "visible";
+      }, 100);     
     }
 }
 
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+
 
 // Get the element with id="defaultOpen" and click on it
 // document.getElementById("defaultOpen").click();
 
 function toggleTheme(event) {
   var button = document.getElementsByClassName('toggleText')
+  var texts = document.getElementsByClassName('toggletextColor')
+  for(var i=0; i<texts.length; i++) {
+    texts[i].classList.toggle("TextColorSwitch")
+  }
   for(var i=0; i<button.length; i++) {
     if (button[i].innerHTML == 'DARK THEME') {
+        // dark theme
+        document.getElementById('body').classList.toggle("blackBackground")
+        document.getElementById('nav').classList.toggle("blackBackground")
         button[i].innerHTML = "LIGHT THEME"
+        button[i].style.backgroundColor = "white"
+        button[i].style.color = "black"
       }else {
+        // document.getElementById("resume").classList.toggle()
+        document.getElementById('body').classList.toggle("blackBackground")
+        document.getElementById('nav').classList.toggle("blackBackground")
         button[i].innerHTML = "DARK THEME"
+        button[i].style.backgroundColor = "black"
+        button[i].style.color = "white"
+
       }
   }
-
 }
 function OpenNav() {
   // document.getElementById('toggleButton').style.display = "none"
@@ -54,3 +66,17 @@ function CloseNav() {
   // document.getElementById('toggleButton').style.display = "block"
   document.getElementById('overlay').style.visibility = "hidden"
 }
+
+
+
+// navbar shodow on scroll
+// var myNav = document.getElementById("nav");
+
+// window.onscroll = function() {
+//   "use strict";
+//   if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+//     myNav.classList.add("scroll");
+//   } else {
+//     myNav.classList.remove("scroll");
+//   }
+// };
