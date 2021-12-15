28b81db0e83a4f28932bc7133f19f4efb6dd815d
sanath sasi <sanathsasi01@gmail.com>
2021-12-15 19:11:45 +0530

    mail test

--- mail.php
+++ mail.php
@@ -4,13 +4,14 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "sanathsasi01@gmail.com";
-$subject = "Mail From website";
+$subject = "Mail From Porfolio";
 // $txt ="Name : ". $name . "\r\n  Email : " . $email . "\r\n Message :" . $message;
 $email_body = "You have received an email from " .$name. ".\n\n" .$message;
 $headers = "From: " .$email;
 if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+    $mail =  mail($to,$subject,$email_body,$headers);
+    echo $mail;
 }
 //redirect
-header("Location:thankyou.html");
+// header("Location:thankyou.html");
 ?>
